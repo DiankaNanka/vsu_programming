832ba36c226e388bd9a996acc9d0ea06e6898a80
DiankaNanka <diana.dorokhova@bk.ru>
2019-09-17 18:07:07 +0300

    add task 5

--- hw1/Task5.py
+++ hw1/Task5.py
@@ -1,17 +1,14 @@
 print('Введите значения точки X и Y:')
-X=int(input('X: '))
-Y=int(input('Y: '))
-if X!=0 and Y!=0:
-    if X*Y > 0:
-        if X > 0:
-            print('1 четверть')
-        else:
-            print('3 четверть')
-    else:
-        if X > 0:
-            print('4 четверть')
-        else:
-            print('2 четверть')
+X = int(input('X: '))
+Y = int(input('Y: '))
+if X > 0 and Y > 0:
+    print('1 четверть')
+elif X > 0 and Y < 0:
+    print('4 четверть')
+elif X < 0 and Y > 0:
+    print('2 четверть')
+elif X < 0 and Y < 0:
+    print('3 четверть')
 else:
     print ('лежит на оси')
     
