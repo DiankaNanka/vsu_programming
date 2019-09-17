e933b8f9c06b47eb85c1c717f9a9f58b05c7a4e7
DiankaNanka <diana.dorokhova@bk.ru>
2019-09-17 18:05:16 +0300

    add task 3

--- hw1/Task3.py
+++ hw1/Task3.py
@@ -1,7 +1,7 @@
-print('Введите значения X,Y,Z')
-X=int(input('X: '))
-Y=int(input('Y: '))
-Z=int(input('Z: '))
 import math
-print(math.sqrt(X**2+Y**2+Z**2))
+print('Введите значения X,Y,Z')
+X = int(input('X: '))
+Y = int(input('Y: '))
+Z = int(input('Z: '))
+print(math.sqrt(X ** 2 + Y ** 2 + Z ** 2))
 
