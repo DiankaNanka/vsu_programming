a9a525f501d74b3268cbd6d3ace7a1fa3c851a77
DiankaNanka <diana.dorokhova@bk.ru>
2019-09-17 17:54:58 +0300

    add task 1

--- hw1/Task1.py
+++ hw1/Task1.py
@@ -1,6 +1,6 @@
 print('введите 2 числа')
-a=float(input('число 1:'))
-b=float(input('число 2:'))
+a = float(input('число 1:'))
+b = float(input('число 2:'))
 if a > b:
     print('1 число > чем 2')
 else:
