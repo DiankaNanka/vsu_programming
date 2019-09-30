0917e927ca5c7c090597788d88c2f8cc15ee27b7
DiankaNanka <49941574+DiankaNanka@users.noreply.github.com>
2019-09-30 14:53:09 +0300

    add task 2

--- hw2/Task2.py
+++ hw2/Task2.py
@@ -3,5 +3,5 @@ list_num = []
 while num:
    list_num.append(num)
    num = input()
-list_num = (sorted((list_num), reverse = True))
+list_num = (sorted(list_num, reverse = True))
 print("".join(list_num))
