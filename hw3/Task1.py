e83989b2e0f8033a051f9fa09932bcd933ddc0c5
DianaNanka <diana.dorokhova@bk.ru>
2019-10-19 22:39:35 +0300

    add task 1

--- hw3/Task1.py
+++ hw3/Task1.py
@@ -1,22 +1,21 @@
-d = {'+', '-', '/', '*', '//', '%', '**'}
+d = {'+','-','/','*','%'}
 i = input('Введите пример: ')
 s = set(i)
-for x in ['**', '//']:
-    if i.find(x) != -1:
-        index = i.find(x)
+for x in ['**','//']:
+    if x in i:
         sumb = x
         break
 else:
     sumb = list(d & s)[0]
-    index = i.find(sumb)
+index = i.find(sumb)
 
 a = int(i[:index])
 b = int(i[index + len(sumb):])
-d = {sumb == '+': a + b,
-     sumb == '-': a - b,
-     sumb == '/': a / b,
-     sumb == '*': a * b,
-     sumb == '//': a // b,
-     sumb == '%': a % b,
-     sumb == '**': a ** b}
-print(d[True])
+d = {'+': a + b,
+     '-': a - b,
+     '/': a / b,
+     '*': a * b,
+     '%': a % b,
+     '//': a // b,
+     '**': a ** b}
+print(d[sumb])
