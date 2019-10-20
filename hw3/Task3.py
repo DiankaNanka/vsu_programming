8fa214bab9faa6f6200f2f0a7872f123d0e0c0c5
DianaNanka <diana.dorokhova@bk.ru>
2019-10-20 15:15:36 +0300

    add task 3

--- hw3/Task3.py
+++ hw3/Task3.py
@@ -1,14 +1,11 @@
-f1 = f2 = 1
- 
-n = int(input())
- 
-if n < 2:
-    quit()
- 
-print(f1, end=' ')
-print(f2, end=' ')
-for i in range(2, n):
-    f1, f2 = f2, f1 + f2
+def fib(n):
+    f1 = f2 = 1
+    if n < 2:
+        quit()
+    print(f1, end=' ')
     print(f2, end=' ')
- 
-print()
+    for i in range(2, n):
+        f1, f2 = f2, f1 + f2
+        print(f2, end=' ')
+n = int(input())
+print(fib(n))
