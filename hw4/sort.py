695a305778629ea207c51792ab6be68bc364d3d6
DianaNanka <diana.dorokhova@bk.ru>
2019-10-20 19:24:19 +0300

    add sort

--- hw4/sort.py
+++ hw4/sort.py
@@ -1,13 +1,13 @@
 def insertion_sort(nums):
-    # Создаем копию входящей структуры (она может быть не только списком)
+    # Преобразуем к типу список входящую структуру
     list_nums = list(nums)
-    # Граница такая, т.к. будет идти сравнение [i] и [i+1], исключаем выход за список
-    for i in range(len(list_nums) - 1):
+    # Граница такая, т.к. будет идти сравнение [j] и [j+1], исключаем выход за список
+    for i in range(1, len(list_nums)):
         # Нужен итератор, который пойдет в обратном порядке
         j = i
-        while j >= 0 and list_nums[j + 1] < list_nums[j]:
+        while j and (list_nums[j - 1] > list_nums[j]):
             # Меняем местами переменные
-            list_nums[j + 1], list_nums[j] = list_nums[j], list_nums[j + 1]
+            list_nums[j - 1], list_nums[j] = list_nums[j], list_nums[j - 1]
             # Шаг назад
             j -= 1
     return(list_nums)
