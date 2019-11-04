dde35e97c4ba54799a37a9161b8d251329d4085f
DianaNanka <diana.dorokhova@bk.ru>
2019-11-04 03:13:59 +0300

    add task 1

---
+++ hw6/Task1.py
@@ -0,0 +1,44 @@
+hash_size = 7
+hash_table = [[] for _ in range(hash_size)]
+ 
+ 
+def hash_func(hash_str):
+    return len(hash_str) % hash_size
+ 
+ 
+def set_value(key, value):
+    el = hash_table[hash_func(key)]
+    for pair in el:
+        if key == pair[0]:
+            pair[1] = value
+            break
+    else:
+        el.append([key, value])
+ 
+ 
+def get_value(key):
+    for k, v in hash_table[hash_func(key)]:
+        if key == k:
+            return v
+ 
+ 
+set_value("abc", 1)
+set_value("zxc", 2)
+set_value("fd", 3)
+set_value("a", 4)
+set_value("d", 5)
+set_value("2134", 6)
+set_value("142155", 7)
+ 
+set_value("abc", 999999999999)
+print(get_value("abc"))
+print(get_value("zxc"))
+print(get_value("fd"))
+print(get_value("a"))
+print(get_value("d"))
+print(get_value("2134"))
+print(get_value("142155"))
+print(get_value("142155e"))
+ 
+for x in hash_table:
+    print(x)
