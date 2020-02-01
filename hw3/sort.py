7bc8554f66912b0e73215269c27ee214fbaebc4b
DianaNanka <diana.dorokhova@bk.ru>
2020-02-01 23:50:18 +0300

    Task1_var3

---
+++ hw3/sort.py
@@ -0,0 +1,17 @@
+def insertion_sort(nums):
+    # Преобразуем к типу список входящую структуру
+    list_nums = list(nums)
+    # Граница такая, т.к. будет идти сравнение [j] и [j - 1], исключаем выход за список
+    for i in range(1, len(list_nums)):
+        # Нужен итератор, который пойдет в обратном порядке
+        j = i
+        while j and (list_nums[j - 1] > list_nums[j]):
+            # Меняем местами переменные
+            list_nums[j - 1], list_nums[j] = list_nums[j], list_nums[j - 1]
+            # Шаг назад
+            j -= 1
+    return(list_nums)
+
+
+nums = input()
+print(insertion_sort(nums))
---
+++ test.py
@@ -0,0 +1,36 @@
+
+from collections import deque
+
+
+
+
+
+def dota_pleyer(x):
+    return not len(x) % 2 and x[0] == "D"
+
+    
+
+
+name = {
+    "Valera": ["Egor", "Karl"],
+    "Egor": ["Arsen", "Daha"],
+    "Karl": ["Dima", "Ola", "Sveta"]
+}
+
+
+
+
+
+d = deque(name["Valera"])
+r = []
+
+while name:
+    chelik = d.popleft()
+    if chelik not in r:
+        if dota_pleyer(chelik):
+            print(chelik)
+            break
+        else:
+            d += name.get(chelik, [])
+
+        r.append(chelik)
